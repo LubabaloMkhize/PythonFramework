dd3ffd5f88d1107dcc0b06c0dfddf5acccf93d1a
LubabaloMkhize <lubabalo.mkhize@gmail.com>
2025-11-12 20:26:46 +0200

    updated git ignore

--- testCases/conftest.py
+++ testCases/conftest.py
@@ -6,9 +6,7 @@ from selenium.webdriver.edge.options import Options as EdgeOptions
 from selenium.webdriver.chrome.service import Service as ChromeService
 from selenium.webdriver.firefox.service import Service as FirefoxService
 from selenium.webdriver.edge.service import Service as EdgeService
-from webdriver_manager.chrome import ChromeDriverManager
-from webdriver_manager.firefox import GeckoDriverManager
-from webdriver_manager.microsoft import EdgeChromiumDriverManager
+
 
 @pytest.fixture()
 def setup(browser):
